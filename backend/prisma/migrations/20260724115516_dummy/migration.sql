60b7b278fb5ed1fdd2d6f1b04ab60041a8aa6483
Daniel Miskolczi <221767475+Daniel-Miskolczi@users.noreply.github.com>
2026-07-24 18:52:46 +0200

    feat: DATABASE DESIGN DRAFT 1, removed dummy dataset and route

--- backend/prisma/migrations/20260724115516_dummy/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Dummy" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "balance" DECIMAL(14,2) NOT NULL DEFAULT 0,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Dummy_pkey" PRIMARY KEY ("id")
-);
