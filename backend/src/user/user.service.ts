bd528ea8810ee854c741dafdf7880ec6defc263e
Raffael Wolf <146560011+RaffaelW@users.noreply.github.com>
2026-09-12 18:52:21 +0200

    feat!: add auth endpoints to change username and password and delete the own user and update fronted API service (#38)

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -21,6 +21,17 @@ export async function createUser(name: string, hashedPassword: string) {
   });
 }
 
+export async function updateUser(id: number, name?: string, hashedPassword?: string) {
+  const data: { name?: string; hashPassword?: string } = {};
+  if (name) data.name = name;
+  if (hashedPassword) data.hashPassword = hashedPassword;
+
+  return await prisma.appUser.update({
+    where: { id },
+    data,
+  });
+}
+
 export async function deleteUser(id: number) {
   return await prisma.appUser.delete({
     where: { id },
